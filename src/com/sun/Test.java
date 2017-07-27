64c4f79ecee4fe3a7504eae73018c850b02acf3a
[chenglij] <[lij.cheng@sunyard.com]>
2017-07-27 13:36:00 +0800

    修改Test.java

--- src/com/sun/Test.java
+++ src/com/sun/Test.java
@@ -3,7 +3,7 @@ package com.sun;
 public class Test {
 
 	public static void main(String[] args) {
-		String s = "abc";
+		String s = "abcd";
 		System.out.println("Hello Git");
 		System.out.println(s);
 		System.out.println("Hello World");
