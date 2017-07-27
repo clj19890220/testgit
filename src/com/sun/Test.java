f1c83bdf307e4f68e586db7806f361075f8af8d2
[chenglij] <[lij.cheng@sunyard.com]>
2017-07-27 12:49:48 +0800

    修改Test.java

--- src/com/sun/Test.java
+++ src/com/sun/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		String s = "abc";
-		System.out.println("Hello Gits");
+		System.out.println("Hello Git");
 		System.out.println(s);
-
+		System.out.println("Hello World");
 	}
 
 }
