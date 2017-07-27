ce2f337f99807579508413226b0aabafd44b5b14
clj19890220 <30452014+clj19890220@users.noreply.github.com>
2017-07-27 11:34:40 +0800

    Update Demo.java

--- src/com/sun/Demo.java
+++ src/com/sun/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		String s = "Hello Github";
 		System.out.println(s);
-		Integer i = 2;
+		Integer i = 3;
 		System.out.println(i);
 	}
 
