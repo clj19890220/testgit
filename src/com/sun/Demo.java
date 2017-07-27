663359f6e72f3e16201dda0657b7bf7e1b354ead
[chenglij] <[lij.cheng@sunyard.com]>
2017-07-27 11:52:56 +0800

    修改Demo.java

--- src/com/sun/Demo.java
+++ src/com/sun/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		String s = "Hello Github";
 		System.out.println(s);
-		Integer i = 5;
+		Integer i = 6;
 		System.out.println(i);
 	}
 
